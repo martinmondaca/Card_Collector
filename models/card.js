519afe5eab808ee0144b182e4626415ce53609a3
Glimpz <abgabbard@gmail.com>
2021-03-01 21:20:24 -0700

    created cards and userscards models as well as cards route

---
+++ models/card.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {
+    var cards = sequelize.define("cards", {
+        cardnumber: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1]
+        },
+        setname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1]
+        },
+        cardyear: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            len: [1]
+        },
+        createdAt: {
+            type: DataTypes.DATE(3),
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+            field: 'created_at',
+          },
+          updatedAt: {
+            type: DataTypes.DATE(3),
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+            field: 'updated_at',
+          },
+    }, {
+        freezeTableName: true
+    });
+
+    // Item.associate = function(models) {
+    //     Item.belongsTo(models.User, {
+    //         foreignKey: {
+    //             allowNull: false
+    //         }
+    //     });
+    // };
+    return cards;
+}
