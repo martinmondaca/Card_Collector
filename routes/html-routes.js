f056e494a5064b20a1fad9f8d82d2cef51295dc4
Martin M <martin7mondaca@gmail.com>
2021-03-01 21:24:40 -0700

    added route to take user to page with card collections

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -24,6 +24,13 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"))
   });
+
+  app.get("/collections", isAuthenticated, (req, res) => {
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "../public/members.html"))
+  });
+
 };
