11ef042e62c545c47b2ef515de2825d57175e474
Glimpz <abgabbard@gmail.com>
2021-03-02 17:51:00 -0700

    added newCardRow function in member.js to create new rows for each card

--- public/js/members.js
+++ public/js/members.js
@@ -1,3 +1,32 @@
+function newCardRow(data) {
+  var tableBody = $("#tableBody")
+  var tableRow =`<tr>
+  <td id="cardnumber">${data.cardnumber}</td>
+  <td id="playername">${data.name}</td>
+  <td id="isowned">
+    <div class="mdc-form-field">
+      <div class="mdc-checkbox">
+        <input type="checkbox"
+               class="mdc-checkbox__native-control"
+               id="checkbox-1"/>
+        <div class="mdc-checkbox__background">
+          <svg class="mdc-checkbox__checkmark"
+               viewBox="0 0 24 24">
+            <path class="mdc-checkbox__checkmark-path"
+                  fill="none"
+                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+          </svg>
+          <div class="mdc-checkbox__mixedmark"></div>
+        </div>
+        <div class="mdc-checkbox__ripple"></div>
+      </div>
+    </div>
+  </td>
+  </tr>`
+  tableBody.append(tableRow)
+}
+
+
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
