34f875e7ed7ba49277c492a2ce6c49f22d2911f3
Martin M <martin7mondaca@gmail.com>
2021-03-04 23:56:28 -0700

    added hbs for displaying landing page after user signs in

--- public/js/members.js
+++ public/js/members.js
@@ -1,30 +1,30 @@
-function newCardRow(data) {
-  var tableBody = $("#tableBody")
-  var tableRow = `<tr>
-  <td id="cardnumber">${data.cardnumber}</td>
-  <td id="playername">${data.name}</td>
-  <td id="isowned">
-    <div class="mdc-form-field">
-      <div class="mdc-checkbox">
-        <input type="checkbox"
-               class="mdc-checkbox__native-control"
-               id="checkbox-1"/>
-        <div class="mdc-checkbox__background">
-          <svg class="mdc-checkbox__checkmark"
-               viewBox="0 0 24 24">
-            <path class="mdc-checkbox__checkmark-path"
-                  fill="none"
-                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
-          </svg>
-          <div class="mdc-checkbox__mixedmark"></div>
-        </div>
-        <div class="mdc-checkbox__ripple"></div>
-      </div>
-    </div>
-  </td>
-  </tr>`
-  tableBody.append(tableRow)
-}
+// function newCardRow(data) {
+//   var tableBody = $("#tableBody")
+//   var tableRow = `<tr>
+//   <td id="cardnumber">${data.cardnumber}</td>
+//   <td id="playername">${data.name}</td>
+//   <td id="isowned">
+//     <div class="mdc-form-field">
+//       <div class="mdc-checkbox">
+//         <input type="checkbox"
+//                class="mdc-checkbox__native-control"
+//                id="checkbox-1"/>
+//         <div class="mdc-checkbox__background">
+//           <svg class="mdc-checkbox__checkmark"
+//                viewBox="0 0 24 24">
+//             <path class="mdc-checkbox__checkmark-path"
+//                   fill="none"
+//                   d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+//           </svg>
+//           <div class="mdc-checkbox__mixedmark"></div>
+//         </div>
+//         <div class="mdc-checkbox__ripple"></div>
+//       </div>
+//     </div>
+//   </td>
+//   </tr>`
+//   tableBody.append(tableRow)
+// }
 
 
 $(document).ready(() => {
@@ -33,6 +33,10 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+
+  // $.get("/members" + )
+
 });
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
