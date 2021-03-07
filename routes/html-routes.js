0214d8d8e1666654545e45a031b1544a3a3f471c
Martin M <martin7mondaca@gmail.com>
2021-03-07 01:53:28 -0700

    updated get route to add a checked key to each card with true or false based on whether the user owns that card or not

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,13 +4,17 @@ const path = require("path");
 const exphbs = require("express-handlebars")
 const Handlebars = require("handlebars")
 
+
+
 // const passport = require("../config/passport");
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+const { doc } = require("prettier");
 
 module.exports = function (app) {
+
   // Set Handlebars as the default templating engine.
   app.engine("handlebars", exphbs({
     defaultLayout: "main",
@@ -49,14 +53,11 @@ module.exports = function (app) {
 
   app.get("/members/:setname/:cardyear", isAuthenticated, (req, res) => {
 
-    console.log(req.params.cardyear)
     var setSearch = req.params.setname;
     var yearSearch = req.params.cardyear;
-    var setSearch = req.params.setname
     db.cards.findAll({
       where: {
         cardyear: yearSearch,
-
         setname: setSearch,
 
       }
@@ -64,26 +65,29 @@ module.exports = function (app) {
       // We have access to the todos as an argument inside of the callback function
       console.log(typeof (data))
       console.log("end of data")
+      // console.log(data)
       db.userscards.findAll({
         where: {
           userId: req.user.id,
         }
-      }).then(info => {
-        var newArray = [];
+      }).then(function (info) {
+        console.log("this is the info")
+        console.log(info)
         var userCards = info.map((element) => element.cardId)
         console.log(userCards)
         for (let index = 0; index < data.length; index++) {
           const card = data[index];
-        if (userCards.includes(card.cardId)) {
-          card.checked = "true";
-        } else  {
-          card.checked = "false"
-        };
-      }
-      console.log(data)
-      res.render("set", { cards: data })
+          if (userCards.includes(card.id)) {
+            card.checked = true;
+          } else {
+            card.checked = false
+          };
+        }
+        console.log(data)
+        //   console.log(data)
+        res.render("set", { cards: data })
+        // console.log(data)
       })
-      
       // res.send(data)
 
       // var currentSet = {
@@ -98,4 +102,61 @@ module.exports = function (app) {
       //res.render("set", { cards: data })
     });
   });
+
+  //ANDREWS CODE
+  // app.get("/members/:setname/:cardyear", isAuthenticated, (req, res) => {
+  //   console.log(req.params.cardyear)
+  //   var setSearch = req.params.setname;
+  //   var yearSearch = req.params.cardyear;
+  //   var setSearch = req.params.setname
+  //   db.cards.findAll({
+  //     where: {
+  //       cardyear: yearSearch,
+  //       setname: setSearch,
+  //     }
+  //   }).then(function (data) { //JASON: this is cards
+  //     // We have access to the todos as an argument inside of the callback function
+  //     // console.log(typeof (data))
+  //     // console.log(data)
+  //     console.log("end of data")
+  //     db.userscards.findAll({
+  //       where: {
+  //         userId: req.user.id,
+  //       }
+  //     }).then(info => { //JASON: this is usercards
+  //       console.log(info)
+  //       // console.log(info)
+  //       data.map(c => { //this is cards
+  //         c.checked = info.filter(uc => uc.cardId == card.Id && uc.userId == req.user.id).length > 0
+  //       })
+  //       // var newArray = [];
+  //       // var userCards = info.map((element) => element.cardId)
+  //       // console.log(userCards)
+  //       // for (let index = 0; index < data.length; index++) {
+  //       //     const card = data[index];
+  //       //     if (userCards.includes(card.cardId)) {
+  //       //         card.checked = "true";
+  //       //     } else {
+  //       //         card.checked = "false"
+  //       //     };
+  //       // }
+  //       // console.log(data)
+  //       console.log("heres the data")
+  //       // console.log(data.filter(d => d.checked))
+  //       // res.render("set", { cards: data })
+  //     })
+  //     // res.send(data)
+  //     // var currentSet = {
+  //     //   cardInfo: []
+  //     // }
+  //     // for (var i = 0; i < data.length; i++) {
+  //     //   var currentCardData = JSON.parse(data[i]);
+  //     //   currentSet.cardInfo.concat(currentCardData)
+  //     // }
+  //     // var setToRender = currentSet
+  //     //res.render("set", { cards: data })
+  //   });
+  // });
+
+
 };
